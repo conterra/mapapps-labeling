a11ee7bbccac8fafadd8cb3058471f2a8ca7e8c4
Sebastian Holtkamp <S.Holtkamp@conterra.de>
2024-03-21 16:21:00 +0100

    Improve group layer and sublayer support

--- src/main/js/bundles/dn_labeling/LabelingController.ts
+++ src/main/js/bundles/dn_labeling/LabelingController.ts
@@ -18,6 +18,9 @@ import Graphic from "esri/Graphic";
 import Draw from "esri/views/draw/Draw";
 import Point from "esri/geometry/Point";
 import LabelCreator from "./LabelCreator";
+import * as reactiveUtils from "esri/core/reactiveUtils";
+import { Observers, createObservers } from "apprt-core/Observers";
+
 
 import type { InjectedReference } from "apprt-core/InjectedReference";
 import LabelingModel from "./LabelingModel";
@@ -25,44 +28,29 @@ import { MapWidgetModel } from "map-widget/api";
 
 export default class LabelingController {
 
+    private observers?: Observers;
     private drawAction: any; // TODO Typing
     private hoverGraphic?: Graphic;
     private draw?: Draw;
     private labelCreator?: LabelCreator;
     private _fieldLabels: Array<any> = [];
     private _edgeLabels: Array<any> = [];
-    private _labelingModel!: InjectedReference<typeof LabelingModel>;
-    private _mapWidgetModel!: InjectedReference<MapWidgetModel>;
+    private _labelingModel: InjectedReference<typeof LabelingModel>;
+    private _mapWidgetModel: InjectedReference<MapWidgetModel>;
 
     public activate(): void {
-        const model = this._labelingModel;
+        const model = this._labelingModel!;
+        const mapWidgetModel = this._mapWidgetModel!;
 
         this.labelCreator = new LabelCreator(
-            this._mapWidgetModel,
+            mapWidgetModel,
             model.generalization,
             model.textSymbol,
             model.lengthUnit
         );
 
-        const layers = this._mapWidgetModel.map.layers.items.filter((layer: __esri.Layer) => layer.title);
-        model.layers = layers;
-        model.selectedLayer = layers[0];
-
-        model.selectedLayer.when(() => {
-            const fields = model.selectedLayer.fields;
-            fields.forEach((field: __esri.Field, index: number) => {
-                if (!fields[index].prefix) {
-                    fields[index].prefix = `${fields[index].name}: `;
-                }
-                if (!fields[index].postfix) {
-                    fields[index].postfix = "";
-                }
-                model.fields = fields;
-            });
-        });
-
-        model.watch("active", ({ value }) => {
-            if (value) {
+        model.watch("active", ({ value: active }) => {
+            if (active) {
                 this.activateFeatureSelection();
             }
             else {
@@ -70,19 +58,71 @@ export default class LabelingController {
             }
         });
 
-        model.watch("selectedLayer", ({ value }) => {
-            const fields = value.fields;
-            fields.forEach((field: __esri.Field, index: number) => {
-                if (!fields[index].prefix) {
-                    fields[index].prefix = `${fields[index].name}: `;
-                }
-                if (!fields[index].postfix) {
-                    fields[index].postfix = "";
-                }
+        model.watch("selectedLayer", ({ value: layer }) => {
+            layer.when(() => {
+                const fields = layer.fields;
+                fields.forEach((field: __esri.Field, index: number) => {
+                    if (!fields[index].prefix) {
+                        fields[index].prefix = `${fields[index].name}: `;
+                    }
+                    if (!fields[index].postfix) {
+                        fields[index].postfix = "";
+                    }
+                });
+                model.fields = fields;
+                model.selectedFields = [];
             });
-            model.fields = fields;
-            model.selectedFields = [];
         });
+
+        reactiveUtils.watch(
+            () => [mapWidgetModel.map.layers], ([layers]) => {
+                if (this.observers) {
+                    this.observers.clean();
+                }
+                const observers = this.observers = createObservers();
+
+                layers.forEach(layer => {
+                    observers.add(
+                        layer.watch("loaded", loaded => {
+                            this.updateSelectableLayers();
+
+                            if (loaded && layer?.layers?.length >= 1)
+                                layer.layers.forEach(layer => {
+                                    observers.add(
+                                        layer.watch("loaded", () => {
+                                            this.updateSelectableLayers();
+                                        })
+                                    );
+                                });
+
+                            if (loaded && layer?.sublayers?.length >= 1) {
+                                layer.sublayers.forEach(() => {
+                                    observers.add(
+                                        layer.watch("loaded", () => {
+                                            this.updateSelectableLayers();
+                                        })
+                                    );
+                                });
+                            }
+                        })
+                    );
+                });
+            },
+            {
+                initial: true
+            }
+        );
+    }
+
+    private updateSelectableLayers() {
+        const model = this._labelingModel!;
+        const mapWidgetModel = this._mapWidgetModel!;
+
+        const layers = mapWidgetModel.map.layers;
+        const flattenedLayer = this.getFlattenLayers(layers);
+
+        model.layers = flattenedLayer.items.filter((layer: __esri.Layer) => layer.title && layer.type !== "group" && !layer?.sublayers);
+        model.selectedLayer = model.layers[0];
     }
 
     private activateFeatureSelection(): void {
@@ -227,6 +267,10 @@ export default class LabelingController {
                 });
             }
         });
+    }
+
+    private getFlattenLayers(layers: __esri.Collection<__esri.Layer>): __esri.Collection<__esri.Layer> {
+        return layers.flatten(item => item.layers || item.sublayers);
     }
 
 }
